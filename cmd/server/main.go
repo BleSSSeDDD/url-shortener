e81ad97164a8c0b1997c5274d00b7c15aef46258
BleSSSeDDD <111vanya112@gmail.com>
2025-11-10 10:39:27 +0300

    getLinesFromChannel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"time"
 )
 
+func getLinesFromChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer close(out)
+		tmp := make([]byte, 8)
+
+		for {
+			n, err := f.Read(tmp)
+			if n > 0 {
+				out <- string(tmp[:n])
+				time.Sleep(100 * time.Millisecond)
+			}
+			if err != nil {
+				break
+			}
+		}
+	}()
+
+	return out
+}
 func main() {
 	f, err := os.Open("cmd/server/test.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
-	res := make([]byte, 5)
-	for {
-		n, err := f.Read(res)
-		fmt.Printf("[%s]", res[:n])
-		if n < len(res) {
-			fmt.Println(err)
-			break
+
+	go func() {
+		for str := range getLinesFromChannel(f) {
+			fmt.Println(str)
 		}
-	}
+	}()
+	time.Sleep(2 * time.Second)
 
 }
