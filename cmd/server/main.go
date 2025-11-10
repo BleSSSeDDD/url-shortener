1bf63da927c5650efd6a4595d04b4ca7790e2734
BleSSSeDDD <111vanya112@gmail.com>
2025-11-10 09:38:23 +0300

    text reading from file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL Shortener is working! Path: %s", r.URL.Path)
-	})
+	f, err := os.Open("cmd/server/test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := make([]byte, 5)
+	for {
+		n, err := f.Read(res)
+		fmt.Printf("[%s]", res[:n])
+		if n < len(res) {
+			fmt.Println(err)
+			break
+		}
+	}
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-
-	fmt.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
